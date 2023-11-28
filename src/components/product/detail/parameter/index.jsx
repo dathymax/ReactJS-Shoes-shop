c559efe5fc1b2a31af750665de1af616bd1c9e55
Do Tuan Dat <dathymax@gmail.com>
2023-11-28 09:44:14 +0700

    update

--- src/components/product/detail/parameter/index.jsx
+++ src/components/product/detail/parameter/index.jsx
@@ -1,11 +1,30 @@
-import { Button, Divider } from "antd";
+import { Button, Divider, Modal, notification } from "antd";
 import { AiOutlineHeart, AiOutlineShareAlt } from "react-icons/ai";
 import ColorParameter from "./Color";
 import SizeParamter from "./Size";
 import { AiOutlineZoomIn, AiOutlineShoppingCart } from "react-icons/ai";
 import ProductCounter from "../../../counter";
+import { useState } from "react";
+import ProductCartQuickView from "../../cart/QuickView";
 
 const ProductDetailParameter = () => {
+    const [open, setOpen] = useState(false);
+    const [api, contextHolder] = notification.useNotification();
+
+    const openNotificationWithIcon = () => {
+      api["success"]({
+        message: 'Thêm vào giỏ hàng thành công!',
+      });
+    };
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
     return (
         <div className="col-span-6 bg-gray-50 rounded-lg p-6">
             <h3 className="text-[30px] font-medium">Adidas Galaxy 6 Shoes - Blue</h3>
@@ -39,12 +58,49 @@ const ProductDetailParameter = () => {
                 <Button
                     block
                     className="h-[50px] flex items-center justify-center gap-3"
+                    onClick={handleOpen}
                 >
                     <AiOutlineShoppingCart className="text-[16px]" />
                     <p className="font-medium">Thêm vào giỏ hàng</p>
                 </Button>
             </div>
             <Divider />
+
+            <Modal
+                open={open}
+                onCancel={handleClose}
+                destroyOnClose
+                title={"Added to cart"}
+                footer={null}
+                centered
+            >
+                <ProductCartQuickView />
+                <div>
+                <div className="flex items-center justify-between mb-3 text-[20px] mt-5">
+                    <h3>Subtotal</h3>
+                    <p className='font-bold'>
+                        1.200.000 vnd
+                    </p>
+                    </div>
+                    <p>Taxes and shipping fee will be calculated at checkout</p>
+                </div>
+                <Divider />
+
+                <div className="flex items-center justify-center gap-3">
+                    <Button
+                        block
+                        className="h-[50px]"
+                        onClick={() => {
+                            handleClose()
+                            openNotificationWithIcon()
+                        }}
+                    >
+                        Thêm vào giỏ hàng
+                    </Button>
+                </div>
+            </Modal>
+
+            {contextHolder}
         </div>
     );
 };
